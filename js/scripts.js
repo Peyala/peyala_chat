45bbc28ceedea2e1146ac6a8666f511c613f8b5a
Nihelkb <nihelkella@gmail.com>
2022-04-14 02:35:12 +0200

    Gran avance

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function startAnimation(){
+    const drch = document.getElementById('nihel');
+    const izq = document.getElementById('idir');
+    drch.style.opacity = '0';
+    izq.style.transform = 'scale(0.75,0.75) translate(95%,-30%)';
+    console.log('hey');
+}
