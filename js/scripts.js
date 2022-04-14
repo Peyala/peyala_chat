50e13177e2fccd5ae57e83246b8291c9ad3b1cac
Idir Carlos Aliane Crespo <ic.aliane@alumnos.upm.es>
2022-04-14 15:57:34 +0200

    Animaciones completas

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-function startAnimation(){
+function startAnimationIdir(){
     const drch = document.getElementById('nihel');
     const izq = document.getElementById('idir');
+    const pass = document.getElementById('pass_id');
     drch.style.opacity = '0';
-    izq.style.transform = 'scale(0.75,0.75) translate(95%,-30%)';
-    console.log('hey');
+    drch.style.transform = 'scale(0.80,0.80) translate(-85%,-20%)';
+    izq.style.transform = 'scale(0.80,0.80) translate(85%,-20%)';
+    pass.style.opacity = '1';
+    pass.style.transform = 'translate(0%,-450%)';
+}
+
+function startAnimationNihel(){
+    const drch = document.getElementById('nihel');
+    const izq = document.getElementById('idir');
+    const pass = document.getElementById('pass_id');
+    izq.style.opacity = '0';
+    izq.style.transform = 'scale(0.80,0.80) translate(85%,-20%)';
+    drch.style.transform = 'scale(0.80,0.80) translate(-85%,-20%)';
+    pass.style.opacity = '1';
+    pass.style.transform = 'translate(0%,-450%)';
 }
